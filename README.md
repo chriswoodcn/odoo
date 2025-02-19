b194ad7d1f42196a9c65e507e526674ab9b4dded
chriswoodcn <chriswoodcn@aliyun.com>
2025-02-19 22:59:19 +0800

    [update] 更新model EstateProperty;增加部分说明

--- README.md
+++ README.md
@@ -32,6 +32,8 @@ from the documentation.
 To learn the software, we recommend the <a href="https://www.odoo.com/slides">Odoo eLearning</a>, or <a href="https://www.odoo.com/page/scale-up-business-game">Scale-up</a>, the <a href="https://www.odoo.com/page/scale-up-business-game">business game</a>. Developers can start with <a href="https://www.odoo.com/documentation/master/developer/howtos.html">the developer tutorials</a>
 
 ~~~shell
+pip install setuptools wheel
+pip install -r requirements.txt
 # start the Odoo server
 python odoo-bin -c odoo.config --dev=xml
 
--- myaddons/estate/models/estate_property.py
+++ myaddons/estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from datetime import timedelta
 
 
 class EstateProperty(models.Model):
@@ -8,9 +9,13 @@ class EstateProperty(models.Model):
     name = fields.Char(string="名称", required=True)
     description = fields.Text(string="描述")
     postcode = fields.Char(string="邮编")
-    date_availability = fields.Date(string="可售日期")
+    date_availability = fields.Date(
+        string="可售日期",
+        copy=False,
+        default=lambda self: fields.Datetime.now() + timedelta(month=3),
+    )
     expect_price = fields.Float(string="期望价格", required=True)
-    selling_price = fields.Float(string="销售价格")
+    selling_price = fields.Float(string="销售价格", readonly=True, copy=False)
     bedrooms = fields.Integer(string="卧室数量")
     living_area = fields.Integer(string="使用面积")
     facades = fields.Integer(string="面")
@@ -20,4 +25,11 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         string="花园朝向",
         selection=[("north", "北"), ("south", "南"), ("east", "东"), ("west", "西")],
+    )
+    active=fields.Boolean(string="是否归档", default=True)
+    state = fields.Selection(
+        string="状态",
+        selection=[("new", "新"), ("offer_received", "收到报价"), ("offer_accepted", "接受报价"), ("sold", "售出"), ("canceled", "取消")],
+        copy=False,
+        default="new",
     )
